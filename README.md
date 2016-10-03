278eddb00a37686587b92318301c39d6aab0be44
Scott Fabini <sfabini@mbp.local>
2016-10-03 11:53:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Project Title
+
+Building a basic web application with Flask/Python.
+
+## Getting Started
+
+```
+git clone <this repository>
+```
+
+### Prerequisities
+
+Required python packages installed via pip:
+```
+pip install flask
+pip install flask-script
+pip install flask-bootstrap
+pip install flask-wtf
+```
+
+### Installing
+
+A step by step series of examples that tell you have to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+
+## Authors
+
+* **Scott Fabini** - *Initial work* - [Scott](https://github.com/scottfabini)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone who's code was used
+* Inspiration
+* etc
---
+++ flaskApp.py
@@ -0,0 +1,58 @@
+"""
+    Portions of this code may be derived from the book "Flask Web Development" by Miguel Grinberg
+"""
+
+
+import os
+import sqlite3
+from flask import Flask, request, render_template, \
+    session, g, redirect, url_for, abort, flash, Markup
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(__name__)
+    app.config.update(dict(
+        DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+        SECRET_KEY='development key',
+        USERNAME='admin',
+        PASSWORD='default'
+    ))
+    Bootstrap(app)
+    Moment(app)
+    return app
+
+app = create_app()
+
+
+@app.route('/')
+def index(): return render_template('index.html', name='stranger', current_time=datetime.utcnow())
+
+
+@app.route('/bootstrap/<name>')
+def bootstrap(name): return render_template('base.html', name=name)
+
+
+@app.route('/get/', methods=['GET', 'POST'])
+def get():
+    if request.method == 'GET':
+        return 'GET method'
+    else:
+        return 'POST method'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run()

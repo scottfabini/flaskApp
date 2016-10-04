cd10356a681b7cf823e6c1bb532ec506c25f9b3a
sfabini <scott.fabini@gmail.com>
2016-10-04 14:37:05 -0700

    Add SQLite+SQLAlchemy database.  Use this data to create/update the user greeting on /index.html.

--- flaskApp.py
+++ flaskApp.py
@@ -11,73 +11,14 @@ from datetime import datetime
 from flask.ext.sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
-    # validator Required checks that the form is not empty
-    submit = SubmitField('Submit')
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = '2ED35C78BC8F58B54F83A291A1EE4'
-    app.config.from_object(__name__)
-    app.config.update(dict(
-        DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-        SECRET_KEY='development key',
-        USERNAME='admin',
-        PASSWORD='default'
-    ))
-    Bootstrap(app)
-    Moment(app)
-    return app
-
-
-app = create_app()
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if 'name' not in session:
-        session['name'] = None
-    if form.validate_on_submit():
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('index'))
-    return render_template('index.html', name=session['name'], current_time=datetime.utcnow(), form=form)
-
-
-@app.route('/bootstrap/<name>')
-def bootstrap(name): return render_template('base.html', name=name)
-
-
-@app.route('/get/', methods=['GET', 'POST'])
-def get():
-    if request.method == 'GET':
-        return 'GET method'
-    else:
-        return 'POST method'
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
-def create_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-    db = SQLAlchemy(app)
-    return db
-
-
-db = create_db(app)
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SECRET_KEY'] = '2ED35C78BC8F58B54F83A291A1EE4'
+Bootstrap(app)
+Moment(app)
+db = SQLAlchemy(app)
 
 
 class Role(db.Model):
@@ -101,9 +42,55 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    # validator Required checks that the form is not empty
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form,
+                           name=session.get('name'), current_time=datetime.utcnow(), known=session.get('known', False))
+
+
+
+@app.route('/bootstrap/<name>')
+def bootstrap(name): return render_template('base.html', name=name)
+
+
+@app.route('/get/', methods=['GET', 'POST'])
+def get():
+    if request.method == 'GET':
+        return 'GET method'
+    else:
+        return 'POST method'
 
 
 
 
-if __name__ == '__main__':
-    app.run()
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+
+#if __name__ == '__main__':
+#    app.run()
